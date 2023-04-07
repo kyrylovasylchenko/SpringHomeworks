75c1abd5ba1c38c034ada099d1763792b3557fde
kyva22 <tyrtyrforit@gmail.com>
2023-04-07 22:44:56 +0300

    Initial commit

---
+++ src/test/java/com/example/springhomeworks/SpringHomeWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhomeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeWorksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
