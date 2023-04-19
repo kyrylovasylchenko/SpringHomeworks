9213fbf577748b6bb639701bc80995b8ce745012
kyva22 <121287345+kyva22@users.noreply.github.com>
2023-04-19 12:46:07 +0300

    Update RestProductController.java

--- src/main/java/com/example/springhomeworks/rest/RestProductController.java
+++ src/main/java/com/example/springhomeworks/rest/RestProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/product")
+@RequestMapping("/v1/products")
 public class RestProductController {
 
     private final ProductService productService;
