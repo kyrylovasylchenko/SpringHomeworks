c00a93c1292fd5fd742d27ffc8141bd72586dd2a
kyva22 <121287345+kyva22@users.noreply.github.com>
2023-05-02 00:15:29 +0300

    Update ProductRepository.java

--- src/main/java/com/example/springhomeworks/repository/ProductRepository.java
+++ src/main/java/com/example/springhomeworks/repository/ProductRepository.java
@@ -3,5 +3,6 @@ package com.example.springhomeworks.repository;
 import com.example.springhomeworks.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 }
