7bf8cf2df03d781ac1b6c4a3451f6e8b5331b4bf
kyva22 <tyrtyrforit@gmail.com>
2023-06-06 22:33:50 +0300

    VKS_20

---
+++ src/main/java/com/example/springhomeworks/entity/User.java
@@ -0,0 +1,61 @@
+package com.example.springhomeworks.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="users")
+public class User implements UserDetails {
+
+    @Id
+    @Column(value="user_id")
+    private Integer id;
+
+    @Column(value="username")
+    private String name;
+
+    @Column(value="password")
+    private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
